d3f33568d95ac6cd50d03822c0e6e4dda9660521
leeonyou <dldhsdb34@naver.com>
2026-05-07 14:04:35 +0900

    init

---
+++ src/main/java/com/ssafy/ssafy_study/SsafyStudyApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.ssafy_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsafyStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsafyStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ssafy/ssafy_study/SsafyStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.ssafy_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsafyStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
